3725c105cd8b77deaf1fb115551ec41969840b19
ehdghk154 <ehdghk154@naver.com>
2024-03-14 20:22:32 +0900

    Feat: HelloWorldBean 추가

--- src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.hwangdo.myrestfulservice.controller;
 
+import com.hwangdo.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,13 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        /**
+         * bean으로 전달이 되면 스프링에서는 responseBody로 변환되기 때문에 json 형태 값을 가짐
+         */
+        return new HelloWorldBean("Hello World!");
     }
 }
