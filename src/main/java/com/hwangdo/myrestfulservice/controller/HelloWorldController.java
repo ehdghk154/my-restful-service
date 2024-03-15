f9e7e1a5ea7ef86f5a8f75b824c371e228a7021d
ehdghk154 <ehdghk154@naver.com>
2024-03-15 22:18:51 +0900

    Feat: @PathVariable 추가
    helloWorldBeanPathVariable 메서드에서 name을 경로 변수로 받아 반환

--- src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.hwangdo.myrestfulservice.controller;
 
 import com.hwangdo.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +17,13 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        /**
-         * bean으로 전달이 되면 스프링에서는 responseBody로 변환되기 때문에 json 형태 값을 가짐
-         */
+        // bean으로 전달이 되면 스프링에서는 responseBody로 변환되기 때문에 json 형태 값을 가짐
         return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        // bean으로 전달이 되면 스프링에서는 responseBody로 변환되기 때문에 json 형태 값을 가짐
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
