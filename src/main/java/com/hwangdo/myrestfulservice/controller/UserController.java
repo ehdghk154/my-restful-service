6f4b8b7f604f587610385f44483cbd57d4f27edf
ehdghk154 <ehdghk154@naver.com>
2024-03-21 01:25:09 +0900

    Feat: createUser status code 추가
    -ServletUriComponentsBuilder로 경로와 값 URI형태로 저장
    -ResponseEntity.created(URI)로 상태 코드 반환(201 created)

--- src/main/java/com/hwangdo/myrestfulservice/controller/UserController.java
+++ src/main/java/com/hwangdo/myrestfulservice/controller/UserController.java
@@ -2,8 +2,11 @@ package com.hwangdo.myrestfulservice.controller;
 
 import com.hwangdo.myrestfulservice.bean.User;
 import com.hwangdo.myrestfulservice.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
