e042b1a6218d1574710a8cc83f8104cb4179b0a9
ehdghk154 <ehdghk154@naver.com>
2024-03-25 21:04:40 +0900

    Feat: 다국어 처리 기능 연습
    -나라 별 인사말 출력(US,FR,JP,KR)

--- src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/hwangdo/myrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,23 @@
 package com.hwangdo.myrestfulservice.controller;
 
 import com.hwangdo.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    // 생성자 주입
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // GET
     // URI = /hello-world
     // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
@@ -25,5 +36,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         // 경로 변수로 name을 데이터로 받아 로직에 사용
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
