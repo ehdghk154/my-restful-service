f73b03f8876a05bff9c9999391a9ae3551337a8f
ehdghk154 <ehdghk154@naver.com>
2024-03-27 19:38:46 +0900

    Feat: 프로그래밍으로 Filtering 구현
    -이전에는 어노테이션만 사용했다면 이번에는 프로그래밍으로 Filtering 구현
    -id,name,joinDate 외에도 ssn을 추가로 반환하도록 filter 설정

---
+++ src/main/java/com/hwangdo/myrestfulservice/controller/AdminUserController.java
@@ -0,0 +1,73 @@
+package com.hwangdo.myrestfulservice.controller;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.hwangdo.myrestfulservice.bean.AdminUser;
+import com.hwangdo.myrestfulservice.bean.User;
+import com.hwangdo.myrestfulservice.dao.UserDaoService;
+import com.hwangdo.myrestfulservice.exception.UserNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    // 의존성 주입 (생성자 주입)
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }else {
+            BeanUtils.copyProperties(user, adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id","name","joinDate","ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers4Admin() {
+        List<User> users = service.findAll();
+
+        List<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+
+        for(User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id","name","joinDate","ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
